321d01536b1de539b1a75f710d617c5097db0d0e
Yeissan <Kirasha1213@gmail.com>
2026-09-13 22:20:00 +0100

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -410,7 +410,7 @@ function renderSeries(id) {
           <div class="chapter-link disabled">
 
             <span class="chapter-name">
-              Capítulos222
+              ▶ Ver capítulos
             </span>
 
             <span class="chapter-count">
